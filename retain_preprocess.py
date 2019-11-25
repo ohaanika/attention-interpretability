679984993854cbab1fa77ecc74d9119de445dc01
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-11-24 23:30:26 -0500

    preprocess data

--- retain_preprocess.py
+++ retain_preprocess.py
@@ -1,6 +1,8 @@
+import argparse
+import os 
+import re
 import numpy as np
 import pandas as pd
-import re
 import spacy
 import nltk
 import collections
@@ -20,29 +22,25 @@ from keras.layers.embeddings import Embedding
 # nltk.download('stopwords')
 # nltk.download('wordnet')
 
-# define constants
-FOLDER_NAME = 'data/'
-FILE_NAMES = ['-train', '-valid', '-test']
-
-def combine_files(dataset):
-    with open(FOLDER_NAME + dataset + '.txt', 'w') as outfile:
-        for file_name in FILE_NAMES:
-            with open(FOLDER_NAME + dataset + file_name + '.txt') as infile:
+def combine_files():
+    with open(os.path.join(DATA_PATH, DATA_SET, DATA_SET+'.txt'), 'w') as outfile:
+        for split in DATA_SPLITS:
+            with open(os.path.join(DATA_PATH, DATA_SET, DATA_SET+'-'+split+'.txt')) as infile:
                 outfile.write(infile.read())
 
-def get_vocab_size(dataset):
-    with open(FOLDER_NAME + dataset + '.txt') as f:
+def get_vocab_size():
+    with open(os.path.join(DATA_PATH, DATA_SET, DATA_SET+'.txt')) as f:
         c = collections.Counter(f.read().split())
     return len(c.keys())
 
-def get_data(dataset):
-    data = pd.read_csv(FOLDER_NAME + 'IMDB.txt', sep='\t', header = None)
+def get_data():
+    data = pd.read_csv(os.path.join(DATA_PATH, DATA_SET, DATA_SET+'.txt'), sep='\t', header=None)
     data.columns = ['sentence', 'target']
     return data
 
 def process_data(data):
-    # lemmatizer = WordNetLemmatizer()
-    # stemmer = PorterStemmer()
+    stop_words = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
     clean_data = []
     for sentence in data:
         # remove html tags
@@ -57,34 +55,48 @@ def process_data(data):
         sentence = sentence.strip()
         # replace all whitespaces and newlines with one space character
         sentence = re.sub(r'\s+', ' ', sentence)
-        # EXTRA STUFF
+        # tokenize sentence into words
         tokenizer = RegexpTokenizer(r'\w+')
         tokens = tokenizer.tokenize(sentence)
+        # remove stop words
+        filtered_words = [w for w in tokens if len(w) > 2 if not w in stop_words]
+        # lemmatize words
+        lemmatized_words = [lemmatizer.lemmatize(w) for w in filtered_words]
         # append to new list of cleaned sentences 
-        clean_data.append(tokens)
+        clean_data.append(' '.join(lemmatized_words))
     return (clean_data)
-    
-    # def preprocess1(sentence):
-    #     sentence = tokenize.sent_tokenize(sentence)
-    #     return sentence
-
-    # def preprocess2(sentence):
-    #     tokenizer = RegexpTokenizer(r'\w+')
-    #     tokens = tokenizer.tokenize(rem_num)  
-    #     filtered_words = [w for w in tokens if len(w) > 2 if not w in stopwords.words('english')]
-    #     stem_words=[stemmer.stem(w) for w in filtered_words]
-    #     lemma_words=[lemmatizer.lemmatize(w) for w in stem_words]
-    #     return ' '.join(lemma_words)
 
 if __name__ == '__main__':
-    dataset = 'IMDB'
-    # combine_files(dataset)
-    vocab_size = get_vocab_size(dataset)
-    data = get_data(dataset)
+
+    # define parser to parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datapath', type=str, default='data', help='define path to data')
+    parser.add_argument('--dataset', type=str, choices=['IMDB', 'yelp'], required=True, help='select dataset')
+    args = parser.parse_args()
+
+    # define constants
+    DATA_PATH = args.datapath
+    DATA_SET = args.dataset
+    DATA_SPLITS = ['train', 'valid', 'test']
+
+    # prepare data
+    combine_files() # TODO: comment out once dataset files combined into one
+    vocab_size = get_vocab_size()
+    data = get_data()
     data['clean'] = process_data(data['sentence'])
-    print('----- ORIGINAL -----')
+
+    # TODO: remove print statements once testing no longer needed
+    print()
+    print('----- EXAMPLE SENTENCE (ORIGINAL) -----')
+    print()
     print(data['sentence'][3])
     print()
-    print('----- CLEAN -----')
+    print('----- EXAMPLE SENTENCE (CLEAN) -----')
+    print()
     print(data['clean'][3])
-    # print(data.head())
+    print()
+    print('----- DATA FRAME -----')
+    print(data.head())
+    print()
+    print('DATA FRAME SIZE: ' + str(data.shape[0]))
+    print()
