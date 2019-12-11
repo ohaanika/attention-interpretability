7821e9cac6ab7cce256125b2e2e2c498ff652b3b
ohaanika <aanika.rahman@mail.mcgill.ca>
2019-12-11 17:50:33 -0500

    update retain

---
+++ retain-simple/retain_arguments.py
@@ -0,0 +1,85 @@
+import os 
+import re
+import math
+import pickle as pickle
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import nltk
+from nltk import tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('wordnet')
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import load_model, Model
+from keras import layers as L
+from keras import backend as K
+from keras.regularizers import l2
+from keras.constraints import Constraint
+from keras.callbacks import ModelCheckpoint, Callback
+from keras.utils import plot_model
+
+
+class Arguments():
+    def __init__(self, dataset, dir_data, dir_model, preprocessing, stopwords,
+                 emb_size, alpha_rec_size, beta_rec_size, dropout_input, dropout_context, l2,
+                 epochs, batch_size):
+        # directories
+        self.directory = {
+            'data': os.path.join(dir_data, preprocessing + '_' + stopwords),
+            'model': dir_model
+        }
+        # paths
+        self.path = {
+            'data_raw': os.path.join('..', dir_data, dataset, dataset+'.txt'),
+            'data_train': os.path.join(dir_data, dataset, preprocessing + '_' + stopwords + '_stopwords', 'data_train.pkl'),
+            'data_test': os.path.join(dir_data, dataset, preprocessing + '_' + stopwords + '_stopwords', 'data_test.pkl'),
+            'target_train': os.path.join(dir_data, dataset, preprocessing + '_' + stopwords + '_stopwords', 'target_train.pkl'),
+            'target_test': os.path.join(dir_data, dataset, preprocessing + '_' + stopwords + '_stopwords', 'target_test.pkl'),
+            'dictionary': os.path.join(dir_data, dataset, preprocessing + '_' + stopwords + '_stopwords', 'dictionary.pkl'),
+            'model': os.path.join(dir_model, 'weights.01.hdf5'),
+            'model_plot': os.path.join(dir_model, 'model.png'),
+        }
+        # preprocessing methods
+        self.preprocessing = preprocessing # CHOICES: ['lemmatize', 'stem']
+        self.stopwords = stopwords # CHOICES: ['remove', 'include']
+        # vocabulary ids
+        self.pad_id = 0
+        self.oov_id = 1
+        # number of words in vocabulary i.e. self.max_features? self.num_codes?
+        self.num_codes = 150850
+        self.max_features = 150850
+        # maximum number of sentences/words after which the data is truncated i.e. self.n_steps?
+        self.num_sentences = 50
+        self.num_words = 50
+        # size of embedding layer # HYPERPARAMETER
+        self.emb_size = emb_size 
+        # size of recurrent layers # HYPERPARAMETER
+        self.alpha_rec_size = alpha_rec_size 
+        self.beta_rec_size = beta_rec_size 
+        # dropout rate for embedding # HYPERPARAMETER
+        self.dropout_input = dropout_input
+        # dropout rate for context vector # HYPERPARAMETER
+        self.dropout_context = dropout_context
+        # L2 regularitzation value # HYPERPARAMETER
+        self.l2 = l2
+        # number of epochs
+        self.epochs = epochs
+        # batch size
+        self.batch_size = batch_size
+
+
+# initialize arguments
+ARGS = Arguments(dataset='IMDB', dir_data='data', dir_model='model', preprocessing='lemmatize', stopwords='remove',
+                 emb_size=200, alpha_rec_size=200, beta_rec_size=200, dropout_input=0.0, dropout_context=0.0, l2=0.0,
+                 epochs=1, batch_size=128)
+
+
+# set random seed
+# tf.random.set_random_seed(7)
+# np.random.seed(7)
---
+++ retain-simple/retain_preprocess.py
@@ -0,0 +1,177 @@
+from retain_arguments import *
+
+
+def read_data():
+    # read raw data from .txt file
+    data = pd.read_csv(ARGS.path['data_raw'], sep='\t', header=None)
+    # name columns
+    data.columns = ['review', 'target']
+    return data
+
+
+def clean_data(data):
+    stop_words = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    stemmer =  PorterStemmer()
+    print('\nPreprocessing: ' + ARGS.preprocessing + ', ' + ARGS.stopwords + ' stopwords')
+    # parse through reviews in data
+    clean_data = []
+    for review in data:
+        # remove html tags
+        review = re.sub(re.compile('<.*?>'), '\n', review)
+        # tokenize review into sentences
+        review = tokenize.sent_tokenize(review)
+        # parse through sentences in review
+        clean_review = []
+        for sentence in review:
+            # convert to lowercase
+            sentence = sentence.lower()
+            # keep only alphanumeric and space characters
+            sentence = re.sub(r'[^\w\s]', '', sentence)
+            # remove numeric characters
+            sentence = re.sub(r'[0-9]+', '', sentence)
+            # remove spaces at start and end of sentences
+            sentence = sentence.strip()
+            # replace all whitespaces and newlines with one space character
+            sentence = re.sub(r'\s+', ' ', sentence)
+            # tokenize sentence into words
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = tokenizer.tokenize(sentence)
+            # test out different preprocessing methods
+            if ARGS.preprocessing == 'lemmatize':   
+                if ARGS.stopwords == 'remove':
+                    tokens = [lemmatizer.lemmatize(t) for t in tokens if t not in stop_words]
+                else:
+                    tokens = [lemmatizer.lemmatize(t) for t in tokens]
+            elif ARGS.preprocessing == 'remove':
+                if ARGS.stopwords:
+                    tokens = [stemmer.stem(t) for t in tokens if t not in stop_words]
+                else:
+                    tokens = [stemmer.stem(t) for t in tokens]
+            # concatenate words into sentence
+            clean_sentence = ' '.join(tokens)
+            # append clean sentence if its list is not empty
+            if len(clean_sentence):
+                clean_review.append(clean_sentence)
+        # append clean review
+        clean_data.append(clean_review)
+    return clean_data
+
+
+def split_data(df):
+    splits = {}
+    target = pd.DataFrame(data = np.asarray(df['target']), columns=['target'])
+    data = pd.DataFrame(data = np.asarray(df['codes']), columns=['codes'])
+    splits['data_train'], splits['data_test'], splits['target_train'], splits['target_test'] = train_test_split(data, target, test_size=0.3, random_state=7)
+    return splits
+
+
+def encode_data(splits):
+    # initialize tokenizer
+    tokenizer = Tokenizer(oov_token='<OOV>')
+    # initialize data
+    data_train = splits['data_train']['codes']
+    data_test = splits['data_test']['codes']
+    # create internal vocabulary based on train data
+    flat_data_train = [sentence for review in data_train for sentence in review]
+    tokenizer.fit_on_texts(flat_data_train)
+    # transform each sentence in review to a sequence of integers
+    encoded_data_train = data_train.map(lambda s: tokenizer.texts_to_sequences(s))
+    encoded_data_test = data_test.map(lambda s: tokenizer.texts_to_sequences(s))
+    # convert dictionary to desired format
+    word2id = tokenizer.word_index
+    word2id['<PAD>'] = ARGS.pad_id
+    word2id = sorted(word2id.items(), key=lambda x: x[1])
+    id2word = {id: word for word, id in word2id}
+    # replace data with encoded data
+    splits['data_train']['codes'] = encoded_data_train
+    splits['data_test']['codes'] = encoded_data_test
+    return splits, id2word
+
+
+def pad_data(splits):
+    # initialize data (train and test combined) as a list
+    data_both = splits['data_train']['codes'].tolist() + splits['data_test']['codes'].tolist()
+    # note maximum number of sentences in a review / words in a sentence
+    list_num_sentences = [len(review) for review in data_both]
+    max_num_sentences = max(list_num_sentences)
+    print('\nMaximum number of sentences in a review: ' + str(max_num_sentences))
+    print('Length of 10 longest reviews: ' + str(sorted(list_num_sentences, reverse=True)[:10]))
+    list_num_words = [len(sentence) for review in data_both for sentence in review]
+    max_num_words = max(list_num_words)
+    print('\nMaximum number of words in a sentence: ' + str(max_num_words))
+    print('Length of 10 longest sentences: ' + str(sorted(list_num_words, reverse=True)[:10]))
+    # note desired maximum number of sentences in a review / words in a sentence
+    print('\nSet number of sentences in a review after which the data will be truncated: ' + str(ARGS.num_sentences))    
+    print('Set number of words in a sentence after which the data will be truncated: ' + str(ARGS.num_words))
+    # pad reviews to meet desired maximum number of sentences in a review / words in a sentence
+    for split in ['data_train', 'data_test']:
+        data = splits[split]['codes']
+        padded_data = []
+        for review in data:
+            # TODO: decide if we want to pad/truncate post or pre
+            padded_sentences = pad_sequences(review, maxlen=ARGS.num_words, value=ARGS.pad_id,
+                                             padding='post', truncating='post').tolist()
+            if len(padded_sentences) > 50:
+                padded_review = padded_sentences[:50]
+            else:
+                empty_sentences = [[0] * ARGS.num_words for i in range(ARGS.num_sentences-len(padded_sentences))]
+                padded_review = padded_sentences + empty_sentences
+            padded_data.append(padded_review)
+        splits[split]['codes'] = padded_data
+    return splits
+
+
+def pickle_dictionary(dictionary):
+    pd.to_pickle(dictionary, ARGS.path['dictionary'])
+    print('\nDictionary size: ' + str(len(dictionary)))
+    print('\nFirst ten examples from "dictionary":')
+    dictionary_head = {k: dictionary[k] for k in list(dictionary)[:10]}
+    print(dictionary_head)
+
+
+def pickle_splits(splits):
+    for split in splits.keys():
+        pd.to_pickle(splits[split], ARGS.path[split])
+        print('\nData frame for "' + split + '.pkl":')
+        print(splits[split].head())
+
+
+def main(ARGS):
+    print('\n>>> Reading data')
+    df = read_data()
+    print('\nExample review before any preprocessing:')
+    print(df['review'][3])
+
+    print('\n>>> Cleaning data')
+    df['codes'] = clean_data(df['review'])
+    print('\nExample review after cleaning:')
+    print(df['codes'][3])
+
+    print('\n>>> Splitting data into train/test')
+    df_splits = split_data(df)
+
+    print('\n>>> Encoding data')
+    print('\nExample review before encoding:')
+    print(df_splits['data_train']['codes'][3])
+    df_splits, dictionary = encode_data(df_splits)
+    print('\nExample review after encoding:')
+    print(df_splits['data_train']['codes'][3])
+
+    print('\n>>> Padding data')
+    df_splits = pad_data(df_splits)
+
+    print('\n>>> Pickling dictionary')
+    pickle_dictionary(dictionary)
+
+    print('\n>>> Pickling data splits')
+    pickle_splits(df_splits)
+
+
+if __name__ == '__main__':
+    for p in ['lemmatize', 'stem']:
+        for s in ['remove', 'include']:
+            ARGS = Arguments(dataset='IMDB', dir_data='data', dir_model='model', preprocessing=p, stopwords=s,
+                            emb_size=200, alpha_rec_size=200, beta_rec_size=200, dropout_input=0.0, dropout_context=0.0, l2=0.0,
+                            epochs=1, batch_size=128)
+            main(ARGS)
